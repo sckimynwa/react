7e03c13b2279eeeddafaa2dce98d3bb35d38053d
sckimynwa <tsb03672@naver.com>
2022-08-09 09:07:39 +0900

    feat: add sample testcode

---
+++ packages/react-dom/src/__tests__/ReactDOM-yeoul-test.js
@@ -0,0 +1,49 @@
+/**
+ * React Yeoul Test
+ */
+
+'use strict';
+
+let React;
+let ReactDOM;
+
+describe('ReactYeoul', () => {
+  beforeEach(() => {
+    React = require('react');
+    ReactDOM = require('react-dom');
+  });
+
+  it('works', () => {
+    function Button({children}) {
+      return <button>{children}</button>;
+    }
+
+    function Dialog({children}) {
+      return (
+        <div>
+          <div>
+            <div />
+            <p>Hello</p>
+            {children}
+          </div>
+        </div>
+      );
+    }
+
+    function App() {
+      return (
+        <div>
+          <div>
+            <Dialog>
+              <Button>Hello</Button>
+            </Dialog>
+          </div>
+        </div>
+      );
+    }
+    const container = document.createElement('div');
+    ReactDOM.render(<App />, container);
+
+    // expect(container.innerHTML).toBe('<div></div>');
+  });
+});
